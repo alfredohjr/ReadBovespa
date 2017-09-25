97d6422fcf1e978ced944e5b43f3ac792a5f1f78
Alfredo Holz Junior <alfredojrgasper@gmail.com>
2017-09-24 21:38:13 -0300

    Melhorias com casas decimais
    Arrumado as colunas com casa decimais, agora será mostrado corretamente as corretagens, colunas que começam em pred* e a voltot

--- read_bovespa.py
+++ read_bovespa.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+def convert_to_decimals(x):
+    return float(x[:-2] + '.' + x[-2:])
+
 def read_Bovespa(File):
     '''
     this function convert file "COTAHIST_AXXX" to DataFrame
@@ -7,7 +10,16 @@ def read_Bovespa(File):
 
     deli = [2,8,2,12,3,12,10,3,4,13,13,13,13,13,13,13,5,18,18,13,1,8,7,13,11,4]
 
-    df = pd.read_fwf(File,widths=deli,header=None)
+    df = pd.read_fwf(File,widths=deli,header=None,converters={9:convert_to_decimals,
+                                                              10:convert_to_decimals,
+                                                              11:convert_to_decimals,
+                                                              12:convert_to_decimals,
+                                                              13:convert_to_decimals,
+                                                              14:convert_to_decimals,
+                                                              15:convert_to_decimals,
+                                                              18:convert_to_decimals,
+                                                              19:convert_to_decimals,
+                                                                })
 
     df = df.rename(columns={0:'tipreg',1:'data',2:'codbdi',3:'codneg',4:'tpmerc',5:'nomres'
                        ,6:'especi',7:'prazot',8:'modref',9:'preabe',10:'premax'
@@ -17,5 +29,15 @@ def read_Bovespa(File):
     df = df.drop(df.index[[0,df.index.max()]])
 
     df['data'] = pd.to_datetime(df['data'])
-
+    df['datven'] = pd.to_datetime(df['datven'])
+    df['preabe'] = pd.to_numeric(df['preabe'])
+    df['premax'] = pd.to_numeric(df['premax'])
+    df['premin'] = pd.to_numeric(df['premin'])
+    df['premed'] = pd.to_numeric(df['premed'])
+    df['preult'] = pd.to_numeric(df['preult'])
+    df['preofc'] = pd.to_numeric(df['preofc'])
+    df['preofv'] = pd.to_numeric(df['preofv'])
+    df['voltot'] = pd.to_numeric(df['voltot'])
+    df['preexe'] = pd.to_numeric(df['preexe'])
+    
     return df
